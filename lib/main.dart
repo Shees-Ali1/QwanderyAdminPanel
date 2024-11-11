c2f61e0700b5930079febb9f5ff96c7b695abe9e
ali88367 <ali88367a@gmail.com>
2024-11-11 16:10:50 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:iw_admin_panel/firebase_options.dart';
+ import 'package:iw_admin_panel/firebase_options.dart';
 
 import 'adminPanel/MainDashboard.dart';
 
